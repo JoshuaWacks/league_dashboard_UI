e65dcef62369ac07e051dc5ef31839acfbad4e1a
JoshuaWacks <joshuawacks4@gmail.com>
2024-11-07 20:27:14 +0200

    Created sidebar

--- league-dashboard/src/scenes/dashboard/index.jsx
+++ league-dashboard/src/scenes/dashboard/index.jsx
@@ -1,5 +1,13 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+
 const Dashboard = () => {
-    return <div>Dashboard</div>
+    return <Box m= '20px'>
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to Live League Analysis"/>
+        </Box>
+
+    </Box>
 }
 
 export default Dashboard;
